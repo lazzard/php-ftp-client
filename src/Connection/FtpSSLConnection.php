95dcc99f5cd18b003611bde65c9cbcd4f18a0086
elamrani744 <elamrani.sv.laza@gmail.com>
2020-05-20 10:43:13 +0000

    Fix [FtpSSLConnection] (sending the authentication security commands already sends in the built-in login function).

--- src/Connection/FtpSSLConnection.php
+++ src/Connection/FtpSSLConnection.php
@@ -11,20 +11,16 @@
 
 namespace Lazzard\FtpClient\Connection;
 
-use Lazzard\FtpClient\Command\FtpCommand;
 use Lazzard\FtpClient\Exception\ConnectionException;
 
 /**
- * Represents an -Explicit FTP over TLS/TLS- connection.
+ * Represents an -Explicit FTP over TLS/SSL- FTP connection.
  *
  * @since  1.0
  * @author El Amrani Chakir <elamrani.sv.laza@gmail.com>
  */
 class FtpSSLConnection extends FtpConnection
 {
-    /** @var FtpCommand */
-    protected $command;
-
     /**
      * {@inheritDoc}
      *
@@ -33,38 +29,17 @@ class FtpSSLConnection extends FtpConnection
     public function __construct($host, $username, $password, $port = 21, $timeout = 90)
     {
         if (!extension_loaded('openssl')) {
-            throw new ConnectionException("openssl extension not loaded.");
+            throw new ConnectionException("openssl extension required to establish a secure connection, 
+            please enable it.");
         } elseif (!function_exists('ftp_ssl_connect')) {
             throw new ConnectionException("It seems that either the FTP module or openssl extension are not 
-            statically built into your PHP. If you have to use an SSL-FTP connection, then you must compile your 
+            statically built into your PHP. If you have to use an SSL-FTP connection, you must compile your 
             own PHP binaries using the right configuration options.");
         }
 
         parent::__construct($host, $username, $password, $port, $timeout);
     }
 
-    /**
-     * Sends a request to the remote server to secure the data channel.
-     *
-     * @return bool Returns true in success, otherwise throws an exception.
-     *
-     * @throws ConnectionException
-     */
-    public function secureDataChannel()
-    {
-        // TODO check if PBSZ and PROT commands are supported before
-        if (!$this->command->raw(sprintf("PBSZ %s", 0))['code'] !== 200) {
-            $response = $this->command->raw("PROT P");
-
-            if ($response['code'] !== 200) {
-                throw new ConnectionException("Securing data channel was failed.");
-            }
-        } else {
-            throw new ConnectionException("Unable to set buffer connection size.");
-        }
-
-        return true;
-    }
 
     /**
      * @inheritDoc
@@ -73,43 +48,11 @@ class FtpSSLConnection extends FtpConnection
     {
         if (!$this->stream = $this->wrapper->sslConnect($this->getHost(), $this->getPort(), $this->getTimeout())) {
             throw new ConnectionException(ConnectionException::getFtpServerError()
-                ?: "SSL connection failed to FTP server.");
+                ?: "SSL connection failed to the FTP server.");
         }
 
-        $this->command = new FtpCommand($this);
         $this->wrapper->setConnection($this);
 
         return $this->stream;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function login()
-    {
-        // TODO check if the authentication mechanism is supported before
-        if (!$this->tlsAuthentication()) {
-            if (!$this->sslAuthentication()) {
-                throw new ConnectionException("Authentication TLS/SSL was failed.");
-            }
-        }
-
-        return parent::login();
-    }
-
-    /**
-     * @return bool|string
-     */
-    protected function tlsAuthentication()
-    {
-        return ($this->command->raw('AUTH TLS')['code'] === 234);
-    }
-
-    /**
-     * @return bool|string
-     */
-    protected function sslAuthentication()
-    {
-        return ($this->command->raw('AUTH SSL')['code'] === 234);
     }
 }
